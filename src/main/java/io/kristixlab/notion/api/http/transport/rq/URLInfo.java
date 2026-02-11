e5300b78e5307f63665ef80a38e968b842d769b6
kristaxlab <109619441+kristixlab@users.noreply.github.com>
2026-02-11 17:22:23 +0100

    url info fix

--- src/main/java/io/kristixlab/notion/api/http/transport/rq/URLInfo.java
+++ src/main/java/io/kristixlab/notion/api/http/transport/rq/URLInfo.java
@@ -1,29 +1,95 @@
 package io.kristixlab.notion.api.http.transport.rq;
 
-import io.kristixlab.notion.api.http.transport.util.URLInfoBuilder;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import lombok.Data;
 
 @Data
 public class URLInfo {
 
+  private String baseUrl;
   private String url;
   private Map<String, List<String>> queryParams = new HashMap<>();
   private Map<String, String> pathParams = new HashMap<>();
 
-  public static URLInfoBuilder builder() {
-    return new URLInfoBuilder();
+  public static Builder builder() {
+    return new Builder();
   }
 
-  public static URLInfoBuilder builder(String url) {
-    return new URLInfoBuilder().url(url);
+  public static URLInfo.Builder builder(String url) {
+    return new URLInfo.Builder().url(url);
   }
 
-  public static URLInfo build(String url) {
-    URLInfo urlInfo = new URLInfo();
-    urlInfo.setUrl(url);
-    return urlInfo;
+  public static URLInfo from(String url) {
+    return new URLInfo.Builder().url(url).build();
+  }
+
+  public static class Builder {
+
+    private final URLInfo urlInfo = new URLInfo();
+
+    public URLInfo build() {
+      return urlInfo;
+    }
+
+    public Builder url(String url) {
+      urlInfo.setUrl(url);
+      return this;
+    }
+
+    public Builder pathParam(String key, String value) {
+      urlInfo.getPathParams().put(key, value);
+      return this;
+    }
+
+    public Builder pathParams(Map<String, String> pathParams) {
+      urlInfo.getPathParams().putAll(pathParams);
+      return this;
+    }
+
+    public Builder queryParam(String key, List<String> values) {
+      if (urlInfo.getQueryParams().containsKey(key)) {
+        urlInfo.getQueryParams().get(key).addAll(values);
+      } else {
+        urlInfo.getQueryParams().put(key, values);
+      }
+      return this;
+    }
+
+    public Builder queryParam(String key, String value) {
+      queryParam(key, toList(value));
+      return this;
+    }
+
+    public Builder queryParam(String key, Object value) {
+      queryParam(key, toList(String.valueOf(value)));
+      return this;
+    }
+
+    public Builder queryParams(Map<String, String> queryParams) {
+      queryParams.forEach((key, value) -> urlInfo.getQueryParams().put(key, List.of(value)));
+      return this;
+    }
+
+    public Builder queryParamsArrays(Map<String, String[]> queryParams) {
+      queryParams.forEach((key, values) -> urlInfo.getQueryParams().put(key, toList(values)));
+      return this;
+    }
+
+    public Builder queryParamsLists(Map<String, List<String>> queryParams) {
+      urlInfo.getQueryParams().putAll(queryParams);
+      return this;
+    }
+
+    private List<String> toList(String value) {
+      List<String> values = new ArrayList<>();
+      values.add(value);
+      return values;
+    }
+
+    private List<String> toList(String[] array) {
+      List<String> values = new ArrayList<>();
+      Collections.addAll(values, array);
+      return values;
+    }
   }
 }
