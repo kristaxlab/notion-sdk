1100f6a4d2ab1b82bdf2eb39f035ee8ab5ec1c24
kristaxlab <109619441+kristaxlab@users.noreply.github.com>
2026-03-20 18:00:59 +0100

    tests update

--- src/test/java/io/kristixlab/notion/api/examples/IntegrationTest.java
+++ src/test/java/io/kristixlab/notion/api/examples/IntegrationTest.java
@@ -1,7 +1,5 @@
 package io.kristixlab.notion.api.examples;
 
-import io.kristixlab.notion.api.NotionApiClient;
-import io.kristixlab.notion.api.NotionAuthSettings;
 import io.kristixlab.notion.api.http.NotionHttpTransport;
 import io.kristixlab.notion.api.json.JsonConverter;
 import io.kristixlab.notion.api.model.BaseTest;
@@ -18,7 +16,6 @@ public class IntegrationTest extends BaseTest {
 
   private static final String TEST_OUTPUT_DIR = "src/test/resources/notion-api-integration-tests/";
   private static final String TEST_INPUT_DIR = "notion-api-integration-tests/";
-  private static final String TOKEN = "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";
 
   private static NotionHttpTransport transport;
 
@@ -26,12 +23,6 @@ public class IntegrationTest extends BaseTest {
   protected void setUp() throws Exception {
     Path outputDir = Paths.get(TEST_OUTPUT_DIR);
     Files.createDirectories(outputDir);
-    String token = "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";
-    NotionApiClient notionApiClient = new NotionApiClient(TOKEN);
-    // NotionClient notionClient = new NotionClient(token, null);
-    NotionAuthSettings authSettings = new NotionAuthSettings();
-    authSettings.setAccessToken(token);
-    transport = new NotionHttpTransport(authSettings);
   }
 
   protected static String getTestOutputDir() {
