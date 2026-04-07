b90e830a4ee94fb8f7ad743b2dc67f71022bdb24
kristaxlab <109619441+kristaxlab@users.noreply.github.com>
2026-04-07 22:31:39 +0200

    temporarily commented missing classes

--- src/test/java/integration/helper/IntegrationTestAssisstant.java
+++ src/test/java/integration/helper/IntegrationTestAssisstant.java
@@ -5,11 +5,8 @@ import static org.junit.jupiter.api.Assertions.fail;
 import io.kristixlab.notion.api.NotionClient;
 import io.kristixlab.notion.api.model.blocks.BlockList;
 import io.kristixlab.notion.api.model.common.Parent;
-import io.kristixlab.notion.api.model.page.CreatePageParams;
-import io.kristixlab.notion.api.model.page.property.TitleProperty;
 import java.io.File;
 import java.net.URL;
-import java.util.HashMap;
 
 public class IntegrationTestAssisstant {
 
@@ -83,11 +80,12 @@ public class IntegrationTestAssisstant {
   }
 
   public static String createPageForTests(String name, Parent parent) {
-    CreatePageParams createPageParams = new CreatePageParams();
-    createPageParams.setParent(parent);
-    createPageParams.setProperties(new HashMap<>());
-    createPageParams.getProperties().put(TitleProperty.NAME, TitleProperty.of(name));
-    return getNotion().pages().create(createPageParams).getId();
+    //    CreatePageParams createPageParams = new CreatePageParams();
+    //    createPageParams.setParent(parent);
+    //    createPageParams.setProperties(new HashMap<>());
+    //    createPageParams.getProperties().put(TitleProperty.NAME, TitleProperty.of(name));
+    //    return getNotion().pages().create(createPageParams).getId();
+    return null;
   }
 
   private String getTestPageId() {
