395d695322b94e8ec6afd0159dfef011b34a0a7b
kristaxlab <109619441+kristaxlab@users.noreply.github.com>
2026-04-07 21:23:41 +0200

    revert missing class mention

--- src/main/java/io/kristixlab/notion/api/model/common/NotionObject.java
+++ src/main/java/io/kristixlab/notion/api/model/common/NotionObject.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.kristixlab.notion.api.model.BaseNotionObject;
 import io.kristixlab.notion.api.model.blocks.Block;
-import io.kristixlab.notion.api.model.page.Page;
 import io.kristixlab.notion.api.model.users.User;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,7 +14,7 @@ import lombok.Setter;
     property = "object",
     visible = true)
 @JsonSubTypes({
-  @JsonSubTypes.Type(value = Page.class, name = "page"),
+  // @JsonSubTypes.Type(value = Page.class, name = "page"),
   // @JsonSubTypes.Type(value = Database.class, name = "database"),
   // @JsonSubTypes.Type(value = DataSource.class, name = "data_source"),
   // @JsonSubTypes.Type(value = Comment.class, name = "comment"),
