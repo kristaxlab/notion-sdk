37f175d089894d21876fefa649c205f29972d466
kristaxlab <109619441+kristaxlab@users.noreply.github.com>
2026-04-08 16:14:08 +0200

    column blocks update

--- src/main/java/io/kristixlab/notion/api/model/blocks/ColumnListBlock.java
+++ src/main/java/io/kristixlab/notion/api/model/blocks/ColumnListBlock.java
@@ -1,5 +1,8 @@
 package io.kristixlab.notion.api.model.blocks;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,6 +20,44 @@ public class ColumnListBlock extends Block {
   @Getter
   @Setter
   public static class ColumnList {
-    // No properties, just an empty object
+
+    private List<ColumnBlock> children;
+  }
+
+  /** Returns a new builder for constructing a {@link ColumnListBlock}. */
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static class Builder {
+
+    private final ColumnListBlock block;
+
+    private Builder() {
+      block = new ColumnListBlock();
+    }
+
+    public Builder children(Consumer<ColumnBlock.Builder> columnsConsumer) {
+      ColumnBlock.Builder builder = ColumnBlock.builder();
+      columnsConsumer.accept(builder);
+      getChildren().addAll(builder.buildList());
+      return this;
+    }
+
+    public Builder children(List<ColumnBlock> children) {
+      getChildren().addAll(children);
+      return this;
+    }
+
+    private List<ColumnBlock> getChildren() {
+      if (block.getColumnList().getChildren() == null) {
+        block.getColumnList().setChildren(new ArrayList<>());
+      }
+      return block.getColumnList().getChildren();
+    }
+
+    public ColumnListBlock build() {
+      return block;
+    }
   }
 }
